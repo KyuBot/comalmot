f8edb44a730033ae9368ff68e9de32b9c2113e72
mong929 <mong929@naver.com>
2020-11-04 17:42:00 +0900

    [chore] update data

--- comalmot/server/data/gpu.py
+++ comalmot/server/data/gpu.py
@@ -10,10 +10,15 @@ cur = conn.cursor()
 
 driver = webdriver.Chrome('./chromedriver')
 driver.implicitly_wait(1)
-driver.get("http://shop.danawa.com/main/?controller=goods&methods=index&productRegisterAreaGroupSeq=20&serviceSectionSeq=597#1")
-
+driver.get("http://shop.danawa.com/main/?controller=goods&methods=index&productRegisterAreaGroupSeq=20&serviceSectionSeq=597#4")
+emtech = driver.find_elements_by_xpath('//*[@id="makerCode_1"]')
+zotac = driver.find_elements_by_xpath('//*[@id="makerCode_3"]')
+galaxy = driver.find_elements_by_xpath('//*[@id="makerCode_4"]')
+emtech[0].click()
+zotac[0].click()
+galaxy[0].click()
 n = 1
-for p in range(1, 13):
+for p in range(1, 4):
     script = 'movePage(%d)' % p
     driver.execute_script(script)
     time.sleep(5)
@@ -34,16 +39,16 @@ for p in range(1, 13):
                 'dl', attrs={'class': 'low_price'}).find('dd').find('span').text.strip()
             img_src = prod.find('a', attrs={'class': 'thumb_link'}).find('img')[
                 'src']
-            # print(f'{n}. {name} / {price}')
+            print(f'{n}. {name} / {price}')
 
-            # sql = 'INSERT INTO gpus (id, name, price) VALUES (%s, %s, %s)'
-            # data = (n, name, price)
-            # cur.execute(sql, data)
-            # n += 1
-            sql = "UPDATE gpus SET img_src = %s WHERE id = %s"
-            data = (img_src, n)
+            sql = 'INSERT INTO gpus (id, name, price, img_src) VALUES (%s, %s, %s, %s)'
+            data = (n, name, price, img_src)
             cur.execute(sql, data)
             n += 1
+            # sql = "UPDATE gpus SET img_src = %s WHERE id = %s"
+            # data = (img_src, n)
+            # cur.execute(sql, data)
+            # n += 1
 
 conn.commit()
 
--- comalmot/server/data/motherboard.py
+++ comalmot/server/data/motherboard.py
@@ -11,9 +11,16 @@ cur = conn.cursor()
 driver = webdriver.Chrome('./chromedriver')
 driver.implicitly_wait(1)
 driver.get("http://shop.danawa.com/main/?controller=goods&methods=index&productRegisterAreaGroupSeq=20&serviceSectionSeq=595#1")
+btn = driver.find_elements_by_xpath(
+    '//*[@id="searchOptionContainer"]/dl[1]/dd/div/button[1]')
+btn[0].click()
+msi = driver.find_elements_by_xpath('//*[@id="makerCode_2"]')
+biostar = driver.find_elements_by_xpath('//*[@id="makerCode_8"]')
+msi[0].click()
+biostar[0].click()
 
 n = 1
-for p in range(1, 25):
+for p in range(1, 4):
     script = 'movePage(%d)' % p
     driver.execute_script(script)
     time.sleep(5)
@@ -36,14 +43,14 @@ for p in range(1, 25):
                 'src']
             # print(f'{n}. {name} / {price}')
 
-            # sql = 'INSERT INTO motherboards (id, name, price) VALUES (%s, %s, %s)'
-            # data = (n, name, price)
-            # cur.execute(sql, data)
-            # n += 1
-            sql = "UPDATE motherboards SET img_src = %s WHERE id = %s"
-            data = (img_src, n)
+            sql = 'INSERT INTO motherboards (id, name, price, img_src) VALUES (%s, %s, %s, %s)'
+            data = (n, name, price, img_src)
             cur.execute(sql, data)
             n += 1
+            # sql = "UPDATE motherboards SET img_src = %s WHERE id = %s"
+            # data = (img_src, n)
+            # cur.execute(sql, data)
+            # n += 1
 
 conn.commit()
 
