e071de5577b1cd32bde02bf1293b355528a5fdf1
mong929 <mong929@naver.com>
2020-11-14 22:13:41 +0900

    back

--- comalmot/server/app.js
+++ comalmot/server/app.js
@@ -3,13 +3,13 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 // const sequelize = require("./models").sequelize;
 // sequelize.sync();
 
 // app.use(require("connect-history-api-fallback")());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
@@ -23,6 +23,7 @@ const Lreclist = require("./routes/Lreclists");
 const Motherboard = require("./routes/Motherboards");
 const Ram = require("./routes/Rams");
 const Storage = require("./routes/Storages");
+const Chatbot = require("./routes/Chatbots");
 
 app.use("/back/cases", Case);
 app.use("/back/cpus", Cpu);
@@ -33,6 +34,7 @@ app.use("/back/lreclists", Lreclist);
 app.use("/back/motherboards", Motherboard);
 app.use("/back/rams", Ram);
 app.use("/back/storages", Storage);
+app.use("/back/chatbots", Chatbot);
 
 module.exports = app;
 
